e441e1e51840fdb7c839e1b0ab7f08b9fea2f2e5
zhangbao <zhangbao@ly.group>
2021-07-30 18:16:12 +0800

    bugfix

--- drbox/class/categorys/foundation/NSString+drbox.m
+++ drbox/class/categorys/foundation/NSString+drbox.m
@@ -219,7 +219,7 @@
     return [self rangeOfString:string].location != NSNotFound;
 }
 
-- (BOOL)rdr_containsCharacterSet:(NSCharacterSet *)set{
+- (BOOL)dr_containsCharacterSet:(NSCharacterSet *)set{
     if (!set) return NO;
     return [self rangeOfCharacterFromSet:set].location != NSNotFound;
 }
